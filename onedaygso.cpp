5e0b5421620ae39fa580236975418fa6cab9131f
Paul <118366151+Pau1Net@users.noreply.github.com>
2023-07-01 09:19:56 +0300

    Update onedaygso.cpp

--- onedaygso.cpp
+++ onedaygso.cpp
@@ -25,3 +25,36 @@ double OneDayGSO::CountShadowDuration(QDate date)
         return result;
     }
 }
+
+double OneDayGSO::CountShadowDurationByOrbitHeight(double apoapsis, double periapsis)
+{
+    // Расчет радиуса орбиты на основе высоты апоцентра и перицентра
+    double radiusKA = (apoapsis + periapsis) / 2 + radiusEarth;
+
+    // Расчет радиуса тени
+    double radiusShadow = sqrt(pow(radiusKA,2)-pow(radiusEarth,2));
+
+    // Расчет betaT
+    double betaT = radiusShadow / radiusKA;
+
+    // Расчет продолжительности тени
+    double result;
+    if (betaT/sunAngle > 1 || betaT/sunAngle < -1)
+    {
+        result = -1;
+    }
+    else
+    {
+        result = (86164.0/M_PI*acos(betaT/sunAngle));
+    }
+
+    // Создание и отображение метки с результатами
+    QLabel *label = new QLabel;
+    label->setText(QString("Shadow duration for orbit height %1: %2\n")
+                   .arg((apoapsis + periapsis) / 2).arg(result));
+
+    label->show();
+
+    return result;
+}
+
