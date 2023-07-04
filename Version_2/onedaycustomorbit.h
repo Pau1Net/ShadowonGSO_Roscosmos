65838ec50230e1227d214d74a61a73f1c72e1bb8
Paul <118366151+Pau1Net@users.noreply.github.com>
2023-07-04 15:34:20 +0300

    Update onedaycustomorbit.h

--- Version_2/onedaycustomorbit.h
+++ Version_2/onedaycustomorbit.h
@@ -1,8 +1,11 @@
 #ifndef ONEDAYCUSTOMORBIT_H
 #define ONEDAYCUSTOMORBIT_H
 #include <QDate>
+#include <QTime>
+#include <QPair>
 
 static const QDate StartingDate = QDate(2023,3,21);
+static const QTime StartingTime = QTime(9,4);
 
 class OneDayCustomOrbit
 {
@@ -10,8 +13,16 @@ public:
     OneDayCustomOrbit();
     double ReducePosSun(double,double,QDate);
     double CountShadowDuration();
+    QVector<QPair<double, double>> CountShadowDurationM();
+//    QVector<QPair<double, double> > CountShadowDurationM(QVector<double> rka_values);
+    QVector<double> CalculateUS();
+    QVector<double> CalculateTet(QVector<double> us_values, double om);
+    QVector<double> CalculateRka(QVector<double> tet_values, double r_a, double r_p);
 private:
     double exp;
+    double u_s;
+    int hours = StartingTime.hour();
+    int minutes = StartingTime.minute();
 };
 
 #endif // ONEDAYCUSTOMORBIT_H
